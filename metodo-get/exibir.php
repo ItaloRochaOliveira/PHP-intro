907e58f6509ef121317171f9ce740c32ca8e2b4c
ItaloRochaOliveira <italo.rocha.de.oliveira@gmail.com>
2023-07-07 08:45:33 -0300

    metodo-get

---
+++ metodo-get/exibir.php
@@ -0,0 +1,7 @@
+<?php 
+    "<p> Todas as informações recebidas: " . var_dump($_GET) . "</p>";
+
+    echo "<p> O nome é: " . $_GET["nome"] . "</p>" ;
+    echo "<p> A idade é de: " . $_GET["idade"] . "</p>" ;
+    echo "<p> O email é o: " . $_GET["email"] . "</p>" ;
+   
